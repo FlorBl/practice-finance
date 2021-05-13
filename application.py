1e180e38ca98237c4d23e9f02ea667c1cf94517a
Florian <FlorBl>
2021-05-13 19:03:15 -0400

    ad

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_sqlalchemy import SQLAlchemy
 import json
 
 from helpers import apology, login_required, lookup, usd
