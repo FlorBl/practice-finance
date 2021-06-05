162fb6b20881fcbe896dcaa24dadd4fe0f08aea3
Florian <FlorBl>
2021-06-05 17:29:36 -0400

    123;

--- application.py
+++ application.py
@@ -58,7 +58,8 @@ Session.configure(bind=engine)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# work with the session
+session = Session(app)
 
 # Configure CS50 Library to use SQLite database
 #engine = create_engine("sqlite:///finance.db")
