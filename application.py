07fe961995a36f219297d35c00e438265f10c09d
Florian <FlorBl>
2021-06-04 23:00:04 -0400

    tired

--- application.py
+++ application.py
@@ -1,13 +1,20 @@
 import os
 import re
 from cs50 import SQL
+from datetime import datetime #
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 import json
-
+from flask_sqlalchemy import SQLAlchemy #
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import and_, desc
+from decimal import Decimal
+import urllib.request
+import urllib
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
