5a7d5ecd9ce9839fd63f2ea77ef39de4e04be339
Florian <FlorBl>
2021-06-06 09:44:41 -0400

    qwe

--- application.py
+++ application.py
@@ -47,7 +47,7 @@ app.jinja_env.filters["usd"] = usd
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
